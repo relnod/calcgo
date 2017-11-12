b0c9fa9f7849c7ba1bd4e90d5a0159f981c5d140
relnod <mail@paul-schiffers.de>
2017-11-12 11:00:34 +0100

    update benchmark

--- bench_test.go
+++ bench_test.go
@@ -6,8 +6,22 @@ import (
 	"github.com/relnod/calcgo"
 )
 
+var str = "(1 + 2) * 3 + (((2 + 1) * 3 / (5 - 1)) + 1 / 3) - 2 / 3"
+
+func BenchmarkLexer(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		calcgo.Lex(str)
+	}
+}
+
+func BenchmarkParser(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		calcgo.Parse(str)
+	}
+}
+
 func BenchmarkInterpreter(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		calcgo.Interpret("(1 + 2) * 3")
+		calcgo.Interpret(str)
 	}
 }
